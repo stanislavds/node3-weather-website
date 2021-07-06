1c3f74c6afc573a372e3f2859e9d15e3cd393cf4
Stanislav Meshechko DS <stanislav.meshechko@digitalsuits.co>
2021-07-06 17:27:59 +0300

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=2728ce0dd4bf122b4be2d4e64fccff0a&query=${latitude},${longitude}&units=f`;
+
+  request({ url, json: true }, (error, { body } = {}) => {
+    if(error) {
+      callback('Unable to connect to weather services!', undefined);
+    } else if(body.error) {
+      callback('Problem with coords. Try another cords.', undefined)
+    } else {
+      callback(undefined, `Temperature: ${body.current.temperature}. Feel like: ${body.current.feelslike}`)
+    }
+  })
+}
+
+module.exports = forecast
